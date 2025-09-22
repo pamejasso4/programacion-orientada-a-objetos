f0861d929bf3546fa9904ae61b93f0d1fcdde4df
Jose A. Vergara <jvergara@uv.mx>
2025-09-22 15:20:18 -0600

    ejercicio clase 22 de septiembre

---
+++ 22-septiembre/src/shapes/GeometicObject.java
@@ -0,0 +1,48 @@
+package shapes;
+
+import java.util.Date;
+
+class GeometricObject { 
+    private String color;
+    private boolean filled;
+    private Date dateCreated;
+
+    // default constructor
+    GeometricObject() {
+        color = "white";
+        filled = false;
+        dateCreated = new Date();
+    }
+
+    // custom constructor
+    GeometricObject(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+        dateCreated = new Date(); // yyyy-mm-dd hh-mm-ss
+    }
+
+     public String getColor() {
+        return color;
+     }
+     
+     public void setColor(String color) {
+        this.color = color;
+      }
+
+      public boolean isFilled() {
+        return filled;
+      }
+
+      public void setFilled(boolean filled) { 
+        this.filled = filled;
+      }
+
+      public Date getDateCreated() {
+        return dateCreated;
+      }
+
+      @Override
+      public String toString(){
+        return String.format("Color: %s \n Relleno: %b \n Fecha de creacion: %s", color, filled, dateCreated);
+      }
+}
